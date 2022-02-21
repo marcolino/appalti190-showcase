29e698266b6b9a996f7993ae36c1f9d9830210aa
marco <m.solari@mypass.technology>
2022-02-21 19:52:04 +0100

    Commit from package script

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ module.exports = {
     "http://localhost:3000"}/videos/appalti190-crop.mp4`
   ,
   checkoutSessionUrl: `${process.env.NODE_ENV === "production" ? // the checkout session url
-    "https://appalti190-showcase.herokuapp.com" :
+    "https://appalti190.herokuapp.com" :
     "http://localhost:5000"}/create-checkout-session`
   ,
 };
